0bb30c0b2474ea07bfac7f8c1d60aa8ddde928c9
TripEEE <evan.trippel@gmail.com>
2022-09-19 18:15:55 -0400

    Debugging using the assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`❤️❤️❤️  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🥹🥹🥹Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(2, 2);
+assertEqual("LighthouseLabs", "Bootcamp");
