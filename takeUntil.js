28b6da3f87f7677b798a4a048602aff0d324de6e
TripEEE <evan.trippel@gmail.com>
2022-09-22 15:42:34 -0400

    Using a callback function to print a sub-set of arrays to the console.

---
+++ takeUntil.js
@@ -0,0 +1,59 @@
+const _ = require("lodash")
+
+const assertArraysEqual = function(array1, array2) {
+  const _ = require("lodash")
+  const checkArrays = _.isEqual(array1, array2)
+  if(checkArrays) {
+    console.log(`❤️❤️❤️  Assertion Passed: ${array1} === ${array2}`)
+  } else {
+    console.log(`🥹🥹🥹  Assertion Failed: ${array1} === ${array2}`)
+  }
+}
+
+//Create a function called takeUntil
+//It will take in 2 parameters
+  //The array to work with
+  //the callback(which Lodash calls "predicate")
+
+//The function will return a slice of the array with elements taken from the beginning.
+//It should keep going through the array until the callback/predicate returns a truthy value
+//To keep things simple, the callback should only be provided one value: The item in the array.
+
+//PSEUDOCODE:
+// create an empty variable as an expected output
+//use .takeWhile() as the callback, wit
+
+//INPUT:
+
+const takeUntil = function(array, callback) {
+  const output = []
+  for(const item of array) {
+    const status = callback(item)//x on line 49 now represents item, which represents the value of an element in an array
+    if(status) {
+      return output
+    }
+
+    output.push(item)
+  }
+  return output
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5]
+const results1 = takeUntil(data1, x => x < 0)
+console.log(results1)
+
+console.log('---------')
+
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',')
+console.log(results2)
+
+
+//EXPECTED OUTPUT:
+
+// [ 1, 2, 5, 7, 2 ]
+// --
+// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+
+
